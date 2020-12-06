653cbc88d152898b1a88b0dc4bfb778073016b91
Jacob Starry <jstarry@mail.bradley.edu>
2020-12-06 17:11:37 -0600

    Initial Commit

---
+++ KrogerTest/KrogerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrogerTestApp.swift
+//  KrogerTest
+//
+//  Created by Jacob Starry on 12/6/20.
+//
+
+import SwiftUI
+
+@main
+struct KrogerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
